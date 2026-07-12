221dd61721c582c9c283c272179baab502734eb4
christo444 <christoberly2882@gmail.com>
2026-07-12 20:31:17 +0530

    controller files updated

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,3 +1,4 @@
+import Note from "../models/Note.js";
 //.........................
 // export const getAllNotes = (req,res)=>{
 //     res.status(200).send("Heyy mahnn you got all your notes , happyyy");
@@ -5,11 +6,24 @@
 //.........................
 
 export async function getAllNotes(req,res){
-    res.status(200).send("Heyy mahnn you got all your notes , happyyy");
+    // res.status(200).send("Heyy mahnn you got all your notes , happyyy");
+    try {
+        const notes = await Note.find();  //gets all the notes from the dtabase 
+        res.status(200).json(notes);
+    } catch (error) {
+        console.error("error in getallnotes method",error);
+        res.status(500).json({message:"Internal server error"});;
+    }
 }
 
 export async function createNote(req,res){
-    res.status(201).json({message:"note created successfully"});
+    // res.status(201).json({message:"note created successfully"});
+    try {
+        const {title,content} = req.body
+        const newNote =  new Note({title,content})
+    } catch (error) {
+        
+    }
 }
 
 export async function updateNote(req,res){   
