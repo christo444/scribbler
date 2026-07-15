f5366ab3e604569af195377f24c1355088a6ae6b
christo444 <christoberly2882@gmail.com>
2026-07-16 01:29:57 +0530

    navbar files added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Homepage = () => {
     return (
-        <div>Homepage</div>
+        <div className='min-h-screen'>
+            <Navbar />
+        </div>
     )
 }
 
