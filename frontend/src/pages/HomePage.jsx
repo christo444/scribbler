a9e85410f8fd16945bb5bc882284b83ddedcc107
christo444 <christoberly2882@gmail.com>
2026-07-16 01:53:06 +0530

    navbar files added

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
+import RateLimitedUI from '../components/RateLimitedUI';
 
 const Homepage = () => {
+    const [isRateLimited, setIsRateLimited] = useState(true);
     return (
         <div className='min-h-screen'>
             <Navbar />
+            {isRateLimited && <RateLimitedUI />}
         </div>
-    )
-}
+    );
+};
 
-export default Homepage
+export default Homepage;    
